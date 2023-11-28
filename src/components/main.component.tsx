b66203869bad67714d7ada12d878141550ccdee3
Syntarex <4340303+Syntarex@users.noreply.github.com>
2023-11-28 13:26:06 +0100

    Füge Pager hinzu

--- src/components/main.component.tsx
+++ src/components/main.component.tsx
@@ -2,12 +2,14 @@ import { Stack } from "office-ui-fabric-react";
 import * as React from "react";
 import { Header } from "./header/header.component";
 import { List } from "./list/list.component";
+import { Pager } from "./pager/pager.component";
 
 export const Main = () => {
     return (
         <Stack tokens={{ childrenGap: 10 }}>
             <Header />
             <List />
+            <Pager />
         </Stack>
     );
 };
---
+++ src/components/pager/pager.component.tsx
@@ -0,0 +1,63 @@
+import { ceil } from "lodash";
+import { IconButton, Stack, Text } from "office-ui-fabric-react";
+import * as React from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { listInfoSelector } from "../../data/list.data";
+import { pageAtom } from "../../data/ui.data";
+import { webpartPropertiesAtom } from "../../data/webpart.data";
+import styles from "./pager.module.scss";
+
+interface PagerProps {
+    className?: string;
+}
+
+export const Pager = (props: PagerProps) => {
+    const { className } = props;
+
+    const webpartProperties = useRecoilValue(webpartPropertiesAtom);
+    const [page, setPage] = useRecoilState(pageAtom);
+
+    const listInfo = useRecoilValue(listInfoSelector);
+    const pageCount = React.useMemo(
+        () => ceil(listInfo.ItemCount / webpartProperties.pageSize),
+        [webpartProperties, listInfo],
+    );
+
+    const onBackClicked = React.useCallback(() => {
+        // Benutzer befindet sich auf der ersten Seite
+        if (page === 1) {
+            return;
+        }
+
+        setPage(page - 1);
+    }, [page, setPage]);
+
+    const onForwardClicked = React.useCallback(() => {
+        // Benutzer befindet sich auf der letzten Seite
+        if (page === pageCount) {
+            return;
+        }
+
+        setPage(page + 1);
+    }, [page, pageCount, setPage]);
+
+    return (
+        <Stack horizontal horizontalAlign={"center"} verticalAlign={"center"} tokens={{ childrenGap: 20 }}>
+            <Stack.Item>
+                <IconButton disabled={page === 1} iconProps={{ iconName: "ChromeBack" }} onClick={onBackClicked} />
+            </Stack.Item>
+
+            <Stack.Item>
+                <Text className={styles.page}>{page}</Text>
+            </Stack.Item>
+
+            <Stack.Item>
+                <IconButton
+                    disabled={page === pageCount}
+                    iconProps={{ iconName: "ChromeBackMirrored" }}
+                    onClick={onForwardClicked}
+                />
+            </Stack.Item>
+        </Stack>
+    );
+};
