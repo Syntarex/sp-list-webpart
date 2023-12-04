83b91cc55c9232967e7a1d6f9703f42294aee6ac
Syntarex <4340303+Syntarex@users.noreply.github.com>
2023-12-04 14:52:50 +0100

    Füge Error-Funktion hinzu

--- src/util/log.util.ts
+++ src/util/log.util.ts
@@ -5,3 +5,7 @@ export const log = (...data: any) => {
 export const warn = (...data: any) => {
     console.warn("⚠️ list-webpart", ...data);
 };
+
+export const error = (...data: any) => {
+    console.error("‼️ list-webpart", ...data);
+};
